9bddfdf26be9b18fc375fb3e6a2da08aaa977ba4
Alan Racciatti <alanracciatti1220@gmail.com>
2024-05-30 12:19:08 -0300

    fix: build error

--- frontend/app/live/page.tsx
+++ frontend/app/live/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import { useEffect, useRef, useState } from 'react';
-import ReactHlsPlayer from 'react-hls-player';
+import { useRef, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const ReactHlsPlayer = dynamic(() => import('react-hls-player'), { ssr: false });
 
 export default function Live() {
     const playerRef = useRef<HTMLVideoElement | null>(null);
@@ -9,14 +11,14 @@ export default function Live() {
     return (
         <div className='max-w-screen max-h-screen'>
             <ReactHlsPlayer
-            playerRef={playerRef}
-            src="https://cmgw-online-fk.easy4ipcloud.com:8890/LCO/9B0A1B3PBV11415/0/1/20240519T001403/48b0e978d8fdc9acff4b5118a82151af.m3u8?proto=https"
-            autoPlay
-            playsInline
-            width="100%"
-            muted
-            style={{maxHeight: '100vh'}}
+                playerRef={playerRef}
+                src="https://cmgw-online-fk.easy4ipcloud.com:8890/LCO/9B0A1B3PBV11415/0/1/20240519T001403/48b0e978d8fdc9acff4b5118a82151af.m3u8?proto=https"
+                autoPlay
+                playsInline
+                width="100%"
+                muted
+                style={{ maxHeight: '100vh' }}
             />
         </div>
-    )
+    );
 }
